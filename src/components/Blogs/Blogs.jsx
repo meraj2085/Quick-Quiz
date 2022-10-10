3ca03332970b25f8a3d91d0a6dab61b7f6207c6f
meraj2085 <samirmeraj60@gmail.com>
2022-10-11 05:58:24 +0600

    adding footer

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,12 +1,59 @@
-import React from 'react';
+import React from "react";
 
 const Blogs = () => {
-     return (
-          <div>
-               <h1>From blog</h1>
-               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et illum quae accusantium ipsum facilis odit at repellat, nihil itaque unde saepe vero? Beatae excepturi labore dolorum, ipsum at ratione molestias accusamus asperiores, minus alias ad delectus debitis aperiam eum omnis! Aliquam animi voluptates a quibusdam eligendi deleniti illum non quidem eum, totam iure alias? Animi error praesentium dignissimos amet perferendis perspiciatis, blanditiis veritatis nobis distinctio labore! Ex id quibusdam, ipsam temporibus animi suscipit totam corporis quo beatae perspiciatis? Qui facere animi aspernatur rerum tempora, repudiandae temporibus vitae quis provident excepturi, et neque fuga porro alias necessitatibus exercitationem delectus consequatur laudantium.</p>
+  return (
+    <section className="bg-gray-100 text-gray-800">
+      <div className="container flex flex-col justify-center px-4 py-8 mx-auto md:p-8">
+        <h2 className="mb-12 text-4xl font-bold leading-none text-center sm:text-5xl">
+          Frequently Asked Questions
+        </h2>
+        <div className="divide-y divide-gray-300">
+          <div className="py-6 space-y-2 md:grid md:grid-cols-12 md:gap-8 md:space-y-0">
+            <h3 className="font-semibold md:col-span-5">
+              Optio maiores eligendi molestiae totam dolores similique?
+            </h3>
+            <p className="md:pl-0 md:col-span-7">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
+              neque in fugiat magni, quas animi enim veritatis deleniti ex.
+              Impedit.
+            </p>
           </div>
-     );
+          <div className="py-6 space-y-2 md:grid md:grid-cols-12 md:gap-8 md:space-y-0">
+            <h3 className="font-semibold md:col-span-5">
+              Optio maiores eligendi molestiae totam dolores similique?
+            </h3>
+            <p className="md:pl-0 md:col-span-7">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+              aspernatur quae, eos explicabo odit minima libero veniam similique
+              quibusdam doloribus facilis ipsa accusantium vel maiores corrupti!
+              Libero voluptate a doloribus?
+            </p>
+          </div>
+          <div className="py-6 space-y-2 md:grid md:grid-cols-12 md:gap-8 md:space-y-0">
+            <h3 className="font-semibold md:col-span-5">
+              Modi dolorem veritatis culpa quos consequuntur beatae itaque
+              excepturi perspiciatis?
+            </h3>
+            <p className="md:pl-0 md:col-span-7">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
+              voluptates aspernatur dolores in consequatur doloremque inventore
+              reprehenderit, consequuntur perspiciatis architecto.
+            </p>
+          </div>
+          <div className="py-6 space-y-2 md:grid md:grid-cols-12 md:gap-8 md:space-y-0">
+            <h3 className="font-semibold md:col-span-5">
+              Magni reprehenderit possimus debitis?
+            </h3>
+            <p className="md:pl-0 md:col-span-7">
+              Sed consectetur quod tenetur! Voluptatibus culpa incidunt
+              veritatis velit quasi cupiditate unde eaque! Iure, voluptatibus
+              autem eaque unde possimus quae.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Blogs;
