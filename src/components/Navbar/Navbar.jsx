843d8d4e5ea9b5a6ea619e1515d91e3a43560446
meraj2085 <samirmeraj60@gmail.com>
2022-10-11 06:41:40 +0600

    fixing box shadow

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import logo from '../../images/quiz-logo-main.png'
 const Navbar = () => {
 	const [isMenuOpen, setIsMenuOpen] = useState(false)
 	return (
-	  <div className='px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8'>
+	  <div className='px-4 py-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8'>
 	    <div className='relative flex items-center justify-between'>
 		 <Link
 		   to='/'
