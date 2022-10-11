1773cfb245e8dc1378e42dffe3b487be36c5c1ef
meraj2085 <samirmeraj60@gmail.com>
2022-10-11 12:36:21 +0600

    ading active link

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -18,6 +18,12 @@ export const router = createBrowserRouter([
         loader: () => fetch("https://openapi.programming-hero.com/api/quiz"),
       },
       {
+        path: "/home",
+        element: <Home></Home>,
+        loader: () => fetch("https://openapi.programming-hero.com/api/quiz"),
+      },
+      
+      {
         path: "/statistics",
         element: <Statistics></Statistics>,
         loader: () => fetch("https://openapi.programming-hero.com/api/quiz")
