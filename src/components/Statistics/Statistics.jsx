9b6c11cd2ec4d97e905324f6791c795cd5313904
meraj2085 <samirmeraj60@gmail.com>
2022-10-11 11:36:46 +0600

    adding chart responsiveness

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -7,22 +7,25 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
-  Legend,
-  ResponsiveContainer,
 } from "recharts";
 
 const Statistics = () => {
   const data = useLoaderData().data;
   return (
-    <div className="flex justify-center my-32">
-      <div>
-        <LineChart width={700} height={400} data={data}>
-          <Line type="monotone" dataKey="total" stroke="#82ca9d" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          {/* <Legend /> */}
-        </LineChart>
+    <div className="my-5">
+      <div className="text-center mb-8">
+        <h1 className="text-4xl font-semibold">Statistics of total quiz on <br /> every <span className="text-purple-600">topic</span></h1>
+      </div>
+      <div className="flex justify-center mb-36">
+        <div>
+          <LineChart width={450} height={300} data={data}>
+            <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+          </LineChart>
+        </div>
       </div>
     </div>
   );
