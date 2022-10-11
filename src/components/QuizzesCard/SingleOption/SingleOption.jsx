8848626079859a523a6e46a74613dafc7d5aea03
meraj2085 <samirmeraj60@gmail.com>
2022-10-11 12:53:07 +0600

    adding hover effect on quiz btns

--- src/components/QuizzesCard/SingleOption/SingleOption.jsx
+++ src/components/QuizzesCard/SingleOption/SingleOption.jsx
@@ -6,7 +6,7 @@ const SingleOption = ({singleOption, handleCorrectAns}) => {
 
      return (
           <div>
-               <button onClick={()=>handleCorrectAns(singleOption)}  type="button" className="px-8 py-3 font-semibold rounded bg-gray-800 text-gray-100 w-full">{singleOption}</button>
+               <button onClick={()=>handleCorrectAns(singleOption)}  type="button" className="px-8 py-3 font-semibold rounded bg-gray-700 hover:bg-gray-800 text-gray-100 w-full">{singleOption}</button>
           </div>
      );
 };
