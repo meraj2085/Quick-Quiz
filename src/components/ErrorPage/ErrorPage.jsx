5696f8b9658e78c2282f9fba2118e41390566968
meraj2085 <samirmeraj60@gmail.com>
2022-10-11 07:08:50 +0600

    solving class issue

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div class="lg:px-24 lg:py-24 md:py-20 md:px-44 px-5 mt-7 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
-      <div class="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
-        <div class="relative">
-          <div class="absolute">
-            <div class="">
-              <h1 class="my-2 text-gray-800 font-bold text-4xl">
+    <div className="lg:px-24 lg:py-24 md:py-20 md:px-44 px-5 mt-7 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
+      <div className="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
+        <div className="relative">
+          <div className="absolute">
+            <div className="">
+              <h1 className="my-2 text-gray-800 font-bold text-4xl">
               This page does not exist
               </h1>
-              <p class="my-2 text-gray-800">
+              <p className="my-2 text-gray-800">
                 Sorry about that! The page you are looking for could not be found. Please visit our hompage to get where you need
                 to go.
               </p>
--- src/components/TopicsCard/TopicsCard.jsx
+++ src/components/TopicsCard/TopicsCard.jsx
@@ -1,42 +1,42 @@
 import React from "react";
-import { Link, Navigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const TopicsCard = ({ quiz }) => {
   const { id, name, logo, total } = quiz;
 
 
   return (
-    <div class="relative max-w-md mx-auto md:max-w-2xl mt-6 min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-xl mt-16">
-      <div class="px-6">
-        <div class="flex flex-wrap justify-center">
-          <div class="w-full flex justify-center">
-            <div class="relative">
+    <div className="relative max-w-md mx-auto md:max-w-2xl mt-6 min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-xl mt-16">
+      <div className="px-6">
+        <div className="flex flex-wrap justify-center">
+          <div className="w-full flex justify-center">
+            <div className="relative">
               <img
                 src={logo}
                 alt=""
-                class="shadow-xl rounded-full align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-[150px]"
+                className="shadow-xl rounded-full align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-[150px]"
               />
             </div>
           </div>
-          <div class="w-full text-center mt-20">
-            <div class="flex justify-center lg:pt-4 pt-8 pb-0">
-              <div class="p-3 text-center">
-                <span class="text-xl font-bold block uppercase tracking-wide text-slate-700">
+          <div className="w-full text-center mt-20">
+            <div className="flex justify-center lg:pt-4 pt-8 pb-0">
+              <div className="p-3 text-center">
+                <span className="text-xl font-bold block uppercase tracking-wide text-slate-700">
                   {total}
                 </span>
-                <span class="text-sm text-slate-400">Total Quizzes</span>
+                <span className="text-sm text-slate-400">Total Quizzes</span>
               </div>
             </div>
           </div>
         </div>
-        <div class="text-center mt-2">
-          <h3 class="text-2xl text-slate-700 font-bold leading-normal mb-1">
+        <div className="text-center mt-2">
+          <h3 className="text-2xl text-slate-700 font-bold leading-normal mb-1">
             {name}
           </h3>
         </div>
-        <div class="mt-6 py-6 border-t border-slate-200 text-center">
-          <div class="flex flex-wrap justify-center">
-            <div class="w-full px-4">
+        <div className="mt-6 py-6 border-t border-slate-200 text-center">
+          <div className="flex flex-wrap justify-center">
+            <div className="w-full px-4">
             <Link to={`/quizzes/${id}`}>
             <button type="button" className="px-4 py-2 font-semibold rounded-full border-2 border-gray-800 text-gray-800 hover:border-none hover:bg-gray-800 hover:text-gray-100">Start Quiz</button>
             </Link>
